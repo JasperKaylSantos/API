b1eca547547f1012198523c9a1b7f446c7129a86
JasperSantos <JasperKaylSantos@users.noreply.github.com>
2021-05-18 20:17:07 +0800

    1st commit

---
+++ API/POST/Create.java
@@ -0,0 +1,6 @@
+{
+    "name": "morpheus",
+    "job": "leader",
+    "id": "183",
+    "createdAt": "2021-05-18T11:32:47.760Z"
+}
---
+++ API/PUT-PATCH/Update.java
@@ -0,0 +1,5 @@
+{
+    "name": "morpheus",
+    "job": "zion resident",
+    "updatedAt": "2021-05-18T11:36:13.938Z"
+}
